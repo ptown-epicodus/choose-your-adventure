2bdd73cbe1aa90b10f8c88ba1fa85bc64db401da
jay freeman and Patrick McGreevy <jay@>
2017-04-04 10:02:10 -0700

    Add story event model

---
+++ src/app/story-event.model.ts
@@ -0,0 +1,17 @@
+export class StoryEvent {
+  public id: number;
+  public imageLink: string;
+  public choiceDescription: string;
+  public resultDescription: string;
+  public branches: number[];
+  public points: number;
+
+  constructor(
+    public id: number,
+    public choiceDescription: string,
+    public resultDescription: string,
+    public points: number,
+    public imageLink: string,
+    public branches: number[]
+  ) { }
+}
